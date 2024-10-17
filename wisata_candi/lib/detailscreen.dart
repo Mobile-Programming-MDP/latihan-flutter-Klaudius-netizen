d51d9ca031c022a4e52b764c17a653f3c9c5174b
Klaudius-netizen <klaudiusaudieirsansaputra_2327250018@mhs.mdp.ac.id>
2024-10-17 15:10:43 +0700

    anjae

--- wisata_candi/lib/detailscreen.dart
+++ wisata_candi/lib/detailscreen.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/models/candi.dart';
 
 class DetailScreen extends StatelessWidget {
   final Candi candi;
-  const DetailScreen({super.key, required this.ke});
+  const DetailScreen({super.key, required this.candi});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Text('Detail Screen'),
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  candi.imageAsset,
+                  height: 150,
+                  width: double.infinity,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 32.0,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.0),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(onPressed: onPressed, icon: icon),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [Text(data.candi.name)],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Text(candi.name),
+          Text(candi.description)
         ],
       ),
     );
