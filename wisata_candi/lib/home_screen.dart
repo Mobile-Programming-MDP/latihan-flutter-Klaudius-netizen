1b4185b7a6d9db72ced8f4637a47e45bfab20128
Klaudius-netizen <klaudiusaudieirsansaputra_2327250018@mhs.mdp.ac.id>
2024-11-14 14:48:10 +0700

    p

--- wisata_candi/lib/home_screen.dart
+++ wisata_candi/lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/models/candi.dart';
+import 'package:wisata_candi/widgets/item_card.dart'; // Pastikan untuk mengimpor ItemCard
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,19 +12,20 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  Candi candi = candiList[Index];
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Wisata Candi'),
       ),
       body: GridView.builder(
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
         padding: EdgeInsets.all(8),
         itemCount: candiList.length,
-        itemBuilder: (context, Index) {
-          return ItemCard(name: candiList[Index]);
+        itemBuilder: (context, index) {
+          return ItemCard(
+              candi: candiList[index]); // Menggunakan candiList[index]
         },
       ),
     );
