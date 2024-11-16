c48c5718d51fcef62fc1f008ebc5bc8f4d8a87b1
Klaudius-netizen <klaudiusaudieirsansaputra_2327250018@mhs.mdp.ac.id>
2024-11-16 12:21:15 +0700

    Interaction App

---
+++ interaction/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String actionLabel = 'Belum ada aksi';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Interaction'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  actionLabel = 'Pengguna Melakukan Tap';
+                });
+              },
+              onDoubleTap: () {
+                setState(() {
+                  actionLabel = 'Pengguna Melakukan Double Tap';
+                });
+              },
+              onLongPress: () {
+                setState(() {
+                  actionLabel = 'Pengguna Melakukan Long Press';
+                });
+              },
+              child: Container(
+                height: 50,
+                width: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Aksi',
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              actionLabel,
+              style: TextStyle(color: Colors.brown),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
